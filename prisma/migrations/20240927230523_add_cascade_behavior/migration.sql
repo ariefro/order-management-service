e02d8860362c27285e37697f7a13af225c811b1b
arief romadhon <ariefromadhon26@gmail.com>
2024-09-28 06:25:46 +0700

    add onDelete and onUpdate behavior in relations

---
+++ prisma/migrations/20240927230523_add_cascade_behavior/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_customerId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
